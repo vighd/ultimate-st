74cf7d0a776c559bbbe17005f08d621fe77e098f
Vígh Dániel <vighd@digital.co.hu>
2021-08-01 12:37:24 +0200

    Add font2 and boxdraw patch

--- config.h
+++ config.h
@@ -6,6 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "canele:pixelsize=9:antialias=false:autohint=false";
+/* Spare fonts */
+static char *font2[] = {
+	"Waffle:pixelsize=10",
+	"rissole:pixelsize=9",
+	"sundae:pixelsize=9"
+};
 
 /* disable bold, italic and roman fonts globally */
 int disablebold = 1;
@@ -72,6 +78,18 @@ static unsigned int blinktimeout = 800;
  * thickness of underline and bar cursors
  */
 static unsigned int cursorthickness = 2;
+
+/*
+ * 1: render most of the lines/blocks characters without using the font for
+ *    perfect alignment between cells (U2500 - U259F except dashes/diagonals).
+ *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
+ * 0: disable (render all U25XX glyphs normally from the font).
+ */
+const int boxdraw = 1;
+const int boxdraw_bold = 0;
+
+/* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
+const int boxdraw_braille = 0;
 
 /*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
